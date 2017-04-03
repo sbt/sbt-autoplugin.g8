1bbbeeffd90bfbcd8e4b5ef17f5cb9ad63506a37
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-04-03 23:42:28 +0200

    compilable code for the scripted project

---
+++ src/main/g8/src/sbt-test/$name;format="norm"$/simple/src/main/scala/Main.scala
@@ -0,0 +1,17 @@
+/**
+  * A simple class and objects to write tests against.
+  */
+class Main {
+  val default = "the function returned"
+  def method = default + " " + function
+}
+
+object Main {
+
+  val constant = 1
+  def function = 2*constant
+
+  def main(args: Array[String]): Unit = {
+    println(new Main().default)
+  }
+}
